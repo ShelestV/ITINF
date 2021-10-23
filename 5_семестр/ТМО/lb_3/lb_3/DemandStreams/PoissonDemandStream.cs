ca07f252ca29e85f5ac8a3a31106acfb203b5aca
ShelestV <starvova2002@gmail.com>
2021-10-23 21:03:42 +0300

    add streams initializations

--- 5_семестр/ТМО/lb_3/lb_3/DemandStreams/PoissonDemandStream.cs
+++ 5_семестр/ТМО/lb_3/lb_3/DemandStreams/PoissonDemandStream.cs
@@ -1,10 +1,24 @@
+using System;
+using lb_3.Generators;
 using lb_3.Generators.Abstract;
 
 namespace lb_3.DemandStreams
 {
-    public class PoissonDemandStream
+    internal class PoissonDemandStream
     {
         private readonly double streamParameter;
         private readonly IRandomGenerator randomGenerator;
+
+        public PoissonDemandStream(double streamParameter)
+        {
+            this.streamParameter = streamParameter;
+            randomGenerator = new RandomGenerator();
+        }
+
+        public double GetNextWaitingTime()
+        {
+            var randomValue = randomGenerator.NextFrom0To1();
+            return -(Math.Log(randomValue) / streamParameter);
+        }
     }
 }
--- 5_семестр/ТМО/lb_3/lb_3/Program.cs
+++ 5_семестр/ТМО/lb_3/lb_3/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace lb_3
+﻿using lb_3.DemandStreams;
+
+namespace lb_3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int variant = 30;
+            int group = 1;
+            int numberOfChannels = 5; 
+            
+            double requestStreamParameter = 10.0 * group / (variant * numberOfChannels);
+            double responseStreamParameter = 3.0 * (group + variant) / (variant * numberOfChannels);
+
+            var requestStream = new PoissonDemandStream(requestStreamParameter);
+            var responseStreams = new PoissonDemandStream[numberOfChannels];
+
+            for (int i = 0; i < numberOfChannels; ++i)
+            {
+                responseStreams[i] = new PoissonDemandStream(responseStreamParameter);
+            }
+            
             
         }
     }
