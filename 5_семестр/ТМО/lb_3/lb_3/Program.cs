d5935e95ef501e74dcfb3a7dcf8d278c02a93382
vladimirsh <starvova2002@gmail.com>
2021-11-10 12:17:08 +0200

    finish and some times edited for different variants

--- 5_семестр/ТМО/lb_3/lb_3/Program.cs
+++ 5_семестр/ТМО/lb_3/lb_3/Program.cs
@@ -1,15 +1,19 @@
-﻿using lb_3.Collections;
+﻿using System;
+using lb_3.Collections;
 using lb_3.DemandStreams;
+using lb_3.Mathematics;
 
 namespace lb_3
 {
     static class Program
     {
         private const int GROUP = 1;
-        private const int VARIANT = 30;
-        private const int NUMBER_OF_CHANNELS = 5;
+        private const int VARIANT = 9;
+        private const int NUMBER_OF_CHANNELS = 4;
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
             const double requestStreamParameter = 10.0 * GROUP / (VARIANT * NUMBER_OF_CHANNELS);
             const double responseStreamParameter = 3.0 * (GROUP + VARIANT) / (VARIANT * NUMBER_OF_CHANNELS);
 
@@ -25,6 +29,15 @@ namespace lb_3
                 time += requestTime;
                 responseStreams.BusyChannels(time);
             }
+
+            double load = requestStreamParameter / responseStreamParameter;
+            Console.WriteLine();
+            Console.WriteLine("Failure probability: " + Calculation.FailureProbability(load, NUMBER_OF_CHANNELS));
+            Console.WriteLine("Average number of busy channels: " + Calculation.AverageNumberOfBusyChannels(load, NUMBER_OF_CHANNELS));
+            Console.WriteLine("Average number of free channels: " + Calculation.AverageNumberOfFreeChannels(load, NUMBER_OF_CHANNELS));
+            Console.WriteLine("Relative bandwidth: " + Calculation.RelativeBandwidth(load, NUMBER_OF_CHANNELS));
+            Console.WriteLine("Absolute bandwidth: " + Calculation.AbsoluteBandwidth(requestStreamParameter, load, NUMBER_OF_CHANNELS));
+            Console.WriteLine("Busy coefficient: " + Calculation.BusyCoefficient(load, NUMBER_OF_CHANNELS));
         }
     }
 }
